4ce11446866a73e5718bf4b65155b75c23c7e245
Alex Smetanin <AlexSmetanin72@gmail.com>
2022-07-07 17:10:03 +0300

    Spring Framework. Урок 15: Spring MVC. Первое приложение.

---
+++ src/main/java/ua/edu/chpt/springcourse/HelloController.java
@@ -0,0 +1,13 @@
+package ua.edu.chpt.springcourse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "Hello world";
+    }
+}
